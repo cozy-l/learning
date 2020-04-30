523eb243c5a2758be2a5b2379a23dadedd31188d
lrainliu <lrainliu@163.com>
2020-04-30 17:46:02 +0800

    fix

--- gc.py
+++ gc.py
@@ -1,8 +1,8 @@
-#encoding:utf-8
-
 class classA():
+
     def __init__(self):
-        print('object born, id:%s' % str(hex(id(self)))) 
+        print('object born, id:%s' % str(hex(id(self))))
+
     def __del__(self):
-        print('object del,id:%s' %s str(hex(id(self))))
+        print('object del,id:%s' % str(hex(id(self))))
 
